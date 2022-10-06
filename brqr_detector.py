c2cc8d5ebffc0f1f20fef9a3d6816fd9c70a370e
luigisaetta <luigi.saetta@gmail.com>
2022-10-06 08:41:27 +0000

    added func that crops images

--- brqr_detector.py
+++ brqr_detector.py
@@ -1,3 +1,7 @@
+#
+# author:      L. Saetta
+# last update: 03/10/2022
+#
 import torch
 import cv2
 import numpy as np
@@ -19,6 +23,7 @@ class BarcodeQrcodeDetector:
         self.model_path = model_path
         self.confidence = confidence
 
+        # here we load the model. Requires Internet connection to the PyTorch hub
         self._load_model()
 
     # reduce the # of decimal digits
@@ -35,9 +40,29 @@ class BarcodeQrcodeDetector:
 
         return vet
 
+    # this is a utility method.... not to be called from outside
+    def do_crop_for_class(self, results, class_name):
+        # class could be barcode, qrcode barcode o qrcode
+        # results is what returned from model()
+
+        imgs = results.crop(save=False)
+
+        list_imgs_barcode = []
+
+        for img in imgs:
+            if class_name.lower() in img["label"]:
+                im = img["im"]
+                # trasforma l'immagine in RGB altrimenti icolori sono cambiati
+                img_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+                list_imgs_barcode.append(img_rgb)
+
+        return list_imgs_barcode
+
     # the function take as input the img read as np array and returns a matrix
     # with one row for every barcode/qrcode detected and
     # (xmin, ymin, xmax, ymax, confidence, class#, class)
+    # This one return BB rectangles
     def detect_1d2d_codes(self, img: np.ndarray):
         # using TTA
         results = self.model(img, augment=True)
@@ -59,3 +84,27 @@ class BarcodeQrcodeDetector:
         # consider that barcodes are returned in order of decreasing confidence, not by location
         # but you have the BB coords... so you can establish which is above and which is below
         return vet_boxes
+
+    # the function take as input the img read as np array and returns a list
+    # with the images cropped (numpy array, RGB, H,W,C)
+    # This one return images + class_name
+    def detect_and_crop_1d2d_codes(self, img: np.ndarray):
+        # using TTA
+        results = self.model(img, augment=True)
+
+        # first the barcodes and then qrcodes
+        list_barcodes = self.do_crop_for_class(results, "barcode")
+        list_qrcodes = self.do_crop_for_class(results, "qrcode")
+
+        # create a list of (img, class_name)
+        result_list = []
+
+        if len(list_barcodes) > 0:
+            for im in list_barcodes:
+                result_list.append((im, "barcode"))
+
+        if len(list_qrcodes) > 0:
+            for im in list_qrcodes:
+                result_list.append((im, "qrcode"))
+
+        return result_list
